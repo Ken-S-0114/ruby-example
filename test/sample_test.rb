92e7ad01456e1372a03faa7a3795143823391683
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2019-04-26 02:38:10 +0900

    Rakeを理解する

--- test/sample_test.rb
+++ test/sample_test.rb
@@ -2,6 +2,6 @@ require 'minitest/autorun'
 # Minitest は test_ から始まるメソッドを探して実行する
 class SampleTest < Minitest::Test
 	def test_sample
-		assert_equal 'RUBY', nil.upcase
+		# assert_equal 'RUBY', nil.upcase
 	end
 end
